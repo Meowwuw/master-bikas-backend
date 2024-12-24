08d95f5c9364413861a597b610e6fed91a0ec5cb
Magenta <gatichupoderoso@gmail.com>
2024-12-24 10:57:14 -0500

    Enviando token middleware

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,29 +1,31 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-    console.log('verifyToken middleware invocado');
+    console.log("verifyToken middleware invocado");
 
-    const token = req.headers['Authorization'];
-    console.log('Token recibido:', token);
+    const token = req.headers["Authorization"];
+    console.log("Token recibido en el middleware:", token);
 
     if (!token) {
-        console.log('Middleware verifyToken invocado para:', req.method, req.originalUrl);
-        return res.status(403).json({ error: 'Acceso denegado. No se proporcionó un token.' });
+        console.log("Middleware verifyToken invocado para:", req.method, req.originalUrl);
+        return res
+            .status(403)
+            .json({ error: "Acceso denegado. No se proporcionó un token." });
     }
 
     try {
-        const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
-        console.log('Token decodificado:', decoded);
+        const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        console.log("Token decodificado:", decoded);
 
         req.user = {
-            ID_USER: decoded.id, 
+            ID_USER: decoded.id,
             email: decoded.email,
         };
 
         next();
     } catch (error) {
-        console.log('Error al verificar el token:', error.message);
-        res.status(401).json({ error: 'Token inválido o expirado.' });
+        console.log("Error al verificar el token:", error.message);
+        res.status(401).json({ error: "Token inválido o expirado." });
     }
 };
 
