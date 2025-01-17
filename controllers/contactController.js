f7729109a9ba7ce5f9388aac991e2fa2da00e8ad
Magenta <gatichupoderoso@gmail.com>
2025-01-17 00:47:21 -0500

    Enviando hora de peru en bs

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,32 +1,34 @@
 import pool from "../config/db.js";
+import { formatInTimeZone } from "date-fns-tz";
 
 export const createContactRequest = async (req, res) => {
-    const { firstName, lastName, email, phoneNumber } = req.body;
-  
-    if (!firstName || !lastName || !email) {
-      return res.status(400).json({ message: "Faltan datos obligatorios." });
-    }
-  
-    try {
-      const query = `
-        INSERT INTO CONTACT_REQUESTS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, STATUS)
-        VALUES (?, ?, ?, ?, 'PENDIENTE')
-      `;
-      const [result] = await pool.query(query, [
-        firstName,
-        lastName,
-        email,
-        phoneNumber || null,
-      ]);
-  
-      res.status(201).json({
-        message: "Solicitud de contacto creada exitosamente.",
-        requestId: result.insertId,
-      });
-    } catch (error) {
-      console.error("Error al crear la solicitud de contacto:", error);
-      res.status(500).json({ message: "Error interno del servidor." });
-    }
-  };
-  
-  
+  const { firstName, lastName, email, phoneNumber } = req.body;
+
+  if (!firstName || !lastName || !email) {
+    return res.status(400).json({ message: "Faltan datos obligatorios." });
+  }
+
+  try {
+    const currentDateTime = formatInTimeZone(new Date(), "America/Lima", "yyyy-MM-dd HH:mm:ss");
+
+    const query = `
+      INSERT INTO CONTACT_REQUESTS (FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, STATUS, CREATED_AT)
+      VALUES (?, ?, ?, ?, 'PENDIENTE', ?)
+    `;
+    const [result] = await pool.query(query, [
+      firstName,
+      lastName,
+      email,
+      phoneNumber || null,
+      currentDateTime, 
+    ]);
+
+    res.status(201).json({
+      message: "Solicitud de contacto creada exitosamente.",
+      requestId: result.insertId,
+    });
+  } catch (error) {
+    console.error("Error al crear la solicitud de contacto:", error);
+    res.status(500).json({ message: "Error interno del servidor." });
+  }
+};
